242cb74ee9691b131e21210fc56d262ec7c4d0d3
7vtr0x7 <7vtrox7@gmail.com>
2025-03-20 21:59:14 +0530

    feat: clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,31 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+import { tokenCache } from "@/cache";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
+
 function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}></Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}></Stack>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
 
 export default RootLayout;
+``;
