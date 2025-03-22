9dfcd12073e369b6886bbc1a0c635e05b33c025a
7vtr0x7 <7vtrox7@gmail.com>
2025-03-22 18:18:12 +0530

    feat: select image

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,7 +1,53 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import { styles } from "@/styles/create.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+
+import * as ImagePicker from "expo-image-picker";
 
 export default function create() {
+  const [caption, setCaption] = useState<string>("");
+  const [selectedImage, setSelectedImage] = useState<null | string>(null);
+  const [isSharing, setIsSharing] = useState<boolean>(false);
+
+  const router = useRouter();
+  const { user } = useUser();
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: "images",
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.8,
+    });
+
+    if (!result.canceled) setSelectedImage(result.assets[0].uri);
+  };
+
+  if (!selectedImage) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={20} color={COLORS.grey} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>New Post</Text>
+          <View style={{ width: 28 }} />
+        </View>
+
+        <TouchableOpacity
+          style={styles.emptyImageContainer}
+          onPress={() => pickImage()}>
+          <Ionicons name="image-outline" size={48} color={COLORS.grey} />
+          <Text style={styles.emptyImageText}>Tap to select an image</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View>
       <Text>create</Text>
