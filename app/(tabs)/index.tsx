dfa67bfc4913d25cfeba0fc24252e48ec0636e56
7vtr0x7 <7vtrox7@gmail.com>
2025-03-22 22:45:04 +0530

    feat: Story

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,36 @@
 import { useAuth } from "@clerk/clerk-expo";
-import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../../styles/home.styles";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+import styles from "../../styles/feed.styles";
+import { STORIES } from "@/constants/mock-data";
+import Story from "@/components/Story";
 
 const index = () => {
   const { signOut } = useAuth();
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text style={styles.text}>signout</Text>
-      </TouchableOpacity>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>sociallens</Text>
+        <TouchableOpacity
+          onPress={async () => {
+            try {
+              await signOut();
+            } catch (err) {
+              console.log("SignOut Error:", err);
+            }
+          }}>
+          <Ionicons name="log-in-outline" size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator>
+        {STORIES.map((story) => (
+          <Story key={story.id} story={story} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
---
+++ constants/mock-data.ts
@@ -0,0 +1,38 @@
+export const STORIES = [
+    {
+        id: 1,
+        username: "john",
+        avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+        hasStory: false,
+    },
+    {
+        id: 2,
+        username: "emma",
+        avatar: "https://randomuser.me/api/portraits/women/2.jpg",
+        hasStory: true,
+    },
+    {
+        id: 3,
+        username: "michael",
+        avatar: "https://randomuser.me/api/portraits/men/3.jpg",
+        hasStory: true,
+    },
+    {
+        id: 4,
+        username: "sophia",
+        avatar: "https://randomuser.me/api/portraits/women/4.jpg",
+        hasStory: true,
+    },
+    {
+        id: 5,
+        username: "david",
+        avatar: "https://randomuser.me/api/portraits/men/5.jpg",
+        hasStory: true,
+    },
+    {
+        id: 6,
+        username: "olivia",
+        avatar: "https://randomuser.me/api/portraits/women/6.jpg",
+        hasStory: true,
+    },
+];
