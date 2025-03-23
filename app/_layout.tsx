8da0e9858e0d33784cd16ab27c4c54cf267eb216
7vtr0x7 <7vtrox7@gmail.com>
2025-03-23 18:41:53 +0530

    feat: SplashScreen, font & images

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,27 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import InitialLayout from "../components/InitialLayout";
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "black" }}
+          onLayout={onLayoutRootView}>
           <StatusBar style="light" backgroundColor="#000" />
 
           <InitialLayout />
